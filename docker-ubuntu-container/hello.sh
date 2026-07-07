7292948660893df9b0326055aca119fc52e92b7f
MAC <mac@MACs-MacBook-Air.local>
2026-07-07 17:46:25 +0530

    Add Ubuntu Docker container project

---
+++ docker-ubuntu-container/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "=================================="
+echo "Hello from Ubuntu Docker Container"
+echo "Container is running successfully!"
+echo "=================================="
